26b79f10145334bf95a31578fb26a202d4c8e25f
hahahellooo <hahahello777@gmail.com>
2024-12-09 18:36:09 +0900

    -ing

--- src/spark/read_s3.py
+++ src/spark/read_s3.py
@@ -6,12 +6,10 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 load_dotenv()
 
 # Spark 세션 생성
-spark = SparkSession.builder \
-    .appName("Read S3 Data") \
-    .config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
-    .config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("Read S3 Data").getOrCreate()
+    #.config("spark.hadoop.fs.s3a.access.key", os.getenv("AWS_ACCESS_KEY_ID")) \
+    #.config("spark.hadoop.fs.s3a.secret.key", os.getenv("AWS_SECRET_ACCESS_KEY")) \
+    #.config("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com") \
 
 # 예상되는 스키마 정의
 schema = StructType([
@@ -20,7 +18,8 @@ schema = StructType([
 
 try:
     print("Starting to read S3 file...")
-    df = spark.read.format('parquet').load("s3a://t1-tu-data/logs/")
+    #df = spark.read.format('parquet').load("s3a://t1-tu-data/logs/")
+    df = spark.read.format('parquet').schema(schema).load("s3a://t1-tu-data/logs/")
     print("Data preview:")
     df.show()
 except Exception as e:
